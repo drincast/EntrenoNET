51346921dc646e61ba2a60c680d20fb03f4e9f7c
drincastX <drincast@gmail.com>
2018-05-14 17:07:19 -0500

    inicial commit capitulo 2

---
+++ mvctest/mvctest/mvctest/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using mvctest.Models;
+using mvctest.ViewModels;
+
+namespace mvctest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ViewModels.RandomMovieViewModel viewModel;
+
+        public HomeController()
+        {
+            var movie = new Movie() { name = "Destructor" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    name = "customer 01"
+                },
+                new Customer()
+                {
+                    name = "customer 02"
+                },
+            };
+
+            this.viewModel = new RandomMovieViewModel()
+            {
+                customers = customers,
+                movie = movie
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View(viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(this.viewModel);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ mvctest/mvctest/mvctest/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvctest.Models;
+using mvctest.ViewModels;
+
+namespace mvctest.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { name = "Destructor" };
+
+            //ViewData["RandomMovie"] = movie;
+            //ViewBag.Movie2 = movie;
+
+            var viewResult = new ViewResult();
+
+            var customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    name = "customer 01"
+                },
+                new Customer()
+                {
+                    name = "customer 02"
+                },
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                customers = customers,
+                movie = movie
+            };
+
+            //return View(movie);
+            //return Content("Destructor !!");
+            //return Redirect("https://www.google.com");
+            //return Json(movie, JsonRequestBehavior.AllowGet);
+            return View(viewModel);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        } 
+
+        [Route("movies/released/{year}/{month:regex(\\d{2})}") ]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2})}")]
+        public ActionResult test(int year, int month)
+        {
+            var movie = new Movie() { name = "Destructor" };
+
+            ViewData["Movie"] = movie;
+
+            return Content(year + "/" + month);
+        }
+    }
+}
